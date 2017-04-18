9ab0d1a89fac5e3b5f6fdb2b616a8ed851dc2d29
kenichiono <ken_ks35th@yahoo.co.jp>
2017-04-18 02:25:27 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'Task を正常に作成しました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task の作成に失敗しました'
+      render :new
+    end
+  end
+  
+  def edit
+    @message = Message.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'Task を正常に更新しました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task の更新に失敗しました'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  private
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
